dadc9235de00c0a30b5efa27e599543cf37235a0
Dahir Mohamed <dmoabdi98@gmail.com>
2021-12-07 17:30:31 +0000

    readme

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const routes = require("./routes");
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
